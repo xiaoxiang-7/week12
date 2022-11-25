cd2713229260c096663a8eed85b9ae1b8a7d0ed1
xiaoxiang-7 <eric410637340@gmail.com>
2022-11-25 10:25:23 +0800

    make it better

---
+++ index.py
@@ -0,0 +1,56 @@
+from flask import Flask, render_template, request
+import firebase_admin
+from firebase_admin import credentials, firestore
+
+cred = credentials.Certificate("serviceAccountKey.json")
+firebase_admin.initialize_app(cred)
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    homepage = "<h1>資訊管理導論作業</h1>"
+    homepage += "<p>姓名：簡志翔</p>"
+    homepage += "<p>系級：資管三B</p>"
+    homepage += "<p>學號：410637340</p>"
+    #homepage += "<a href=/account>網頁表單輸入實例</a><br><br>"
+    homepage += "<a href=/search>選修課程查詢</a><br><br>"
+    return homepage
+
+@app.route("/account", methods=["GET", "POST"])
+def account():
+    if request.method == "POST":
+        user = request.form["user"]
+        pwd = request.form["pwd"]
+        result = "您輸入的帳號是：" + user + "; 密碼為：" + pwd 
+        return result
+    else:
+        return render_template("account.html")
+
+@app.route("/search", methods=["GET", "POST"])
+def search():
+    if request.method == "POST":
+        cond = request.form["keyword"] #對應read.py第7行的cond代碼
+        result = "您輸入的課程關鍵字是：" + cond
+        
+        db = firestore.client()
+        collection_ref = db.collection("簡志翔電影")
+        docs = collection_ref.get()
+        result = ""
+        for doc in docs:
+            dict = doc.to_dict()
+            if cond in dict["title"]:
+                #print("{}老師開的{}課程,每週{}於{}上課".format(dict["Leacture"], dict["Course"],  dict["Time"],dict["Room"]))
+                result += "片名：" + dict["title"]
+                result += "電影介紹：" + dict["hyperlink"]
+
+            if result =="":
+                #如果查無資料，顯示以下錯誤訊息
+                result = "抱歉，查無相關條件的電影資訊"
+
+        return result
+    else:
+        return render_template("search.html")
+
+#if __name__ == "__main__":
+#    app.run()
